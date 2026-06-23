2fe2fd15c9046d621aefe2eda28e7ae61d3dfc2c
arinkumar820 <147159897+arinkumar820@users.noreply.github.com>
2026-06-23 23:25:39 +0530

    fix typo in title validation transformation in createtodoSchema

--- todo-main/src/Validation/to.validation.js
+++ todo-main/src/Validation/to.validation.js
@@ -7,3 +7,7 @@ const createtodoSchema = z.object({
     description: z.string().min(5, { message: "Description must be at least 5 characters long" }),
     completed: z.boolean().optional(),
 });
+
+
+
+// Export the schema for use in other parts of the application

981e09e3888680ec66748f1b27ff19ee8cd023d9
arinkumar820 <147159897+arinkumar820@users.noreply.github.com>
2026-06-23 23:25:01 +0530

    add trim transformation to title validation in createtodoSchema

--- todo-main/src/Validation/to.validation.js
+++ todo-main/src/Validation/to.validation.js
@@ -3,7 +3,7 @@ import{z}   from "zod";
 
 
 const createtodoSchema = z.object({
-    title: z.string().min(3, { message: "Title must be at least 3 characters long" }),
+    title: z.string().min(3, { message: "Title must be at least 3 characters long" }).trasform((val) => val.trim()),
     description: z.string().min(5, { message: "Description must be at least 5 characters long" }),
     completed: z.boolean().optional(),
 });

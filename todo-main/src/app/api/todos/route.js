f97121e4b3df64203a5e9e558a9132f939309ada
arinkumar820 <147159897+arinkumar820@users.noreply.github.com>
2026-06-23 19:34:05 +0530

    refactor aggregation pipeline in GET function to improve readability and structure

--- todo-main/src/app/api/todos/route.js
+++ todo-main/src/app/api/todos/route.js
@@ -2,20 +2,24 @@ import { NextResponse } from 'next/server'
 import connectDB from '@/lib/mongodb'
 import Todo from '@/models/Todo'
 
+
 export async function GET() {
   try {
     await connectDB()
     const allTodos = await Todo.aggregate([
-      { $match: { completed: true } },
+      { $match: { completed: true }  },
       {
-        //hi everyone
         $group: {
-          _id: null,
-          completedtask: { $sum: 1 }
+          _id: "$completed",
+          completedtask: { $sum: 1 },
         }
       },
       { $sort: { createdAt: -1 } },
-      { $limit: 1 }
+      { project: { _id:0,
+        title:1,
+        category:1,
+        
+       } }
     ])
     const responseData = {
       todos: allTodos,
